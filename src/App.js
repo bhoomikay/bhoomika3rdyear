b6feda1c3dfc7bf6122486c75b3d5051fc6fadc9
bhoomikay <bhoomikay02@gmail.com>
2023-11-20 14:53:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import Header from './Components/Header'
-import Body from './Components/Body'
-import Footer from './Components/Footer'
-const App = () => {
-  return (
-    <div>
-     {/* <Header/> */} 
-      <Body/>
-      {/* <Footer/> */} 
-    </div>
-  )
-}
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Contact from './Components/Contact'
+import Home from './Components/Home'
+import Material from './Components/Material'
+import Todolist from './Components/TodoList'
 
-export default App
+
+ const App= () => {
+  return (
+     <div>
+       <Router>
+         <div>
+        <Material/>
+        </div>
+        <Routes>
+         <Route path='/Home' element={<Home/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/Todolist' element={<Todolist/>}/>
+        
+         </Routes>
+       </Router>
+      
+     </div>
+   )
+ }
+
+ export default App
+
---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+     <phone number>+8050422791</phone><br/>
+     <email>bhoomika02@gmail.com</email><br/>
+     <socialmedia>github:1sv21cs014@shrideviengineering.org</socialmedia><br/> 
+    </div>
+  )
+}
+
+export default Contact
